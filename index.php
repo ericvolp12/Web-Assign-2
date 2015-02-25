b2697c3c6cdef14227a6c825b0952d8b8b1323f9
Eric Volpert <ericvolp12@gmail.com>
2015-02-25 09:22:39 +0000

    Title Update

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ function getQuestions(){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Web Assign 2</title>
 
     <!-- Bootstrap -->
     <link href="styles/bootstrap.css" rel="stylesheet">
